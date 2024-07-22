9556504b02e7434d33f3f123dcc73671384fd023
Omar Calderon <omar.calderon@uabc.edu.mx>
2024-07-21 18:23:39 -0700

    add server files

--- img/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ER4N72JA.mjs";import"./chunk-PWKXGQJZ.mjs";import"./chunk-YYJM75QR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FEQUM3E2.mjs";import"./chunk-PWKXGQJZ.mjs";import"./chunk-YYJM75QR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
