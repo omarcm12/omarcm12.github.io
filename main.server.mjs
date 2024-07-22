ba3e1c27b22fd7fe0d661a44120ddbd17dd9a418
Omar Calderon <omar.calderon@uabc.edu.mx>
2024-07-21 18:38:47 -0700

    update server

--- main.server.mjs
+++ main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FEQUM3E2.mjs";import"./chunk-PWKXGQJZ.mjs";import"./chunk-YYJM75QR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-Y2LC4QXT.mjs";import"./chunk-PWKXGQJZ.mjs";import"./chunk-YYJM75QR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
